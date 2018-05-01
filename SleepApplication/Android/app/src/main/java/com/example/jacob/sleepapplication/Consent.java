e9905e3e7443a2e9165f7b0afb1020eb2440ca5b
esmede14 <esmede14@student.aau.dk>
2018-05-01 16:47:38 +0200

    ConsentView with funcitonal buttons

--- SleepApplication/Android/app/src/main/java/com/example/jacob/sleepapplication/Consent.java
+++ SleepApplication/Android/app/src/main/java/com/example/jacob/sleepapplication/Consent.java
@@ -5,12 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Consent extends AppCompatActivity {
 
     private MainActivity mainActivity;
 
-    public void SetMain (MainActivity mainActivity) {
+    private String consent;
+
+    public void SetMain(MainActivity mainActivity) {
         this.mainActivity = mainActivity;
     }
 
@@ -19,17 +22,35 @@ public class Consent extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.consent);
 
+        final Button btnAccept = (Button) findViewById(R.id.bRegister);
 
+        final Button btnReject = (Button) findViewById(R.id.btnreject);
 
-
-        Button btnreject = (Button) findViewById(R.id.btnreject);
-        btnreject.setOnClickListener(new View.OnClickListener(){
+        btnReject.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                consent = "0";
+                String type = "consent";
+                DatabaseController dbController = new DatabaseController(Consent.this);
+                dbController.execute(type, consent);
+                /*Intent intent = new Intent(Consent.this, MainActivity.class);
+                Consent.this.startActivity(intent);
+            */}
+        });
+
+
+
+/*        btnAccept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                consent = "1";
+                String type = "consent";
+                DatabaseController dbController = new DatabaseController(Consent.this);
+                dbController.execute(type, consent);
                 Intent intent = new Intent(Consent.this, MainActivity.class);
                 Consent.this.startActivity(intent);
             }
-        }) ;
+        });*/
 
     }
 }
