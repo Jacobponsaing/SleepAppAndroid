76821f66f2b0e6593d4c4b320847ea4c71e90e31
esmede14 <esmede14@student.aau.dk>
2018-05-01 13:19:47 +0200

    Started consentView and controller

--- SleepApplication/Android/app/src/main/java/com/example/jacob/sleepapplication/Consent.java
+++ SleepApplication/Android/app/src/main/java/com/example/jacob/sleepapplication/Consent.java
@@ -3,11 +3,11 @@ package com.example.jacob.sleepapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ActivityTwo extends AppCompatActivity {
+public class Consent extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_two);
+        setContentView(R.layout.consent);
     }
 }
