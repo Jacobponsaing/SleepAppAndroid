30667e22fb7a13cae17605d4d0f3bf6d6c7d9a79
esmede14 <esmede14@student.aau.dk>
2018-05-01 14:52:05 +0200

    reject button consent

--- SleepApplication/Android/app/src/main/java/com/example/jacob/sleepapplication/Consent.java
+++ SleepApplication/Android/app/src/main/java/com/example/jacob/sleepapplication/Consent.java
@@ -1,7 +1,10 @@
 package com.example.jacob.sleepapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Consent extends AppCompatActivity {
 
@@ -15,5 +18,15 @@ public class Consent extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.consent);
+
+        Button btnreject = (Button) findViewById(R.id.btnreject);
+        btnreject.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Consent.this, MainActivity.class);
+                Consent.this.startActivity(intent);
+            }
+        }) ;
+
     }
 }
